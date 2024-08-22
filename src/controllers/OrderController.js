237785c8cf44b43283e746f0912747d7c180aaa6
Victorh2002 <victorhugofranciscon@gmail.com>
2024-08-21 22:26:14 -0300

    Diversas telas prototipadas

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -2,6 +2,17 @@ class OrderController{
     createorder( req, res){
         return res.render('criarpedido');
     }
-
+    detalhes(req, res) {
+        return res.render('detalhespedido');
+    }
+    concluidos(req, res) {
+        return res.render('concluidospedido');
+    }
+    cancelados(req, res) {
+        return res.render('canceladospedido');
+    }
+    editar(req, res) {
+        return res.render('editarpedido');
+    }
 }
 export default new OrderController();
--- src/routes/OrderRoute.js
+++ src/routes/OrderRoute.js
@@ -4,4 +4,8 @@ import ordercontroller from "../controllers/OrderController";
 
 const route = new Router();
 route.get('/criar', ordercontroller.createorder);
+route.get('/detalhes', ordercontroller.detalhes);
+route.get('/concluidos', ordercontroller.concluidos);
+route.get('/cancelados', ordercontroller.cancelados);
+route.get('/editar', ordercontroller.editar);
 export default route;
