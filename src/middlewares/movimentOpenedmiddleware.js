3bba91f2577e725e513ee6b553a7bf09514a1a1a
Valdeir de Souza Prestes <valdeirprestes@gmail.com>
2024-08-31 00:13:12 -0300

    Bug do movimento

--- src/middlewares/movimentOpenedmiddleware.js
+++ src/middlewares/movimentOpenedmiddleware.js
@@ -7,16 +7,17 @@ export default async (req, res, next) => {
      
     if(response.data.length > 0){
         res.locals.datamovimento = response.data[0].data;
-        const d = new Date(response.data[0].data);
+        let d = new Date(response.data[0].data);
         const d2 = new Date(response.data[0].data).toLocaleDateString('pt-BR', {timeZone: 'UTC'});;
         res.locals.movimento = d2//.toISOString().split('T')[0]//d.toLocaleDateString(); 
         res.locals.movimento_dia = dia[d.getDay()+1];
         res.locals.movimento_dia_html = d.toISOString().substring(0,10)
         let data_atual = new Date(new Date().toISOString().substring(0,10));
         res.locals.movimentoid = response.data[0].id
-        //console.log("data_atual", data_atual);
-        //console.log("d", d);
-        data_atual = new Date().getTimezoneOffset();
+        
+        d = new Date(d.toISOString().substring(0,10));
+        
+        
         if(data_atual > d )
         {
             
